f3c702132301460b402c372059355242f367e097
Simón Fajardo <132759393+SimonFajardo@users.noreply.github.com>
2023-07-19 20:47:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build)));
 
 //RUTAS
 app.use("/api/v1/auth", authRoutes);
@@ -28,9 +29,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //REST API
-app.get("/", (req, res) => {
-  res.send("<h1>Bienvenido a Hella Store C.A</h1>");
-});
+app.use('*', function (req, res) { res.sendFile(path.join(__dirname, './client/build/index.html));});
 
 //PORT
 const PORT = process.env.PORT || 8080;
